9e8142ba2fc4ce956ddcb09f0e58ad2749d325ad
jhl-hk <valor@jianyuelab.org>
2025-11-10 23:37:03 +0900

    Fix: Style & v0.0.1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,13 @@
 <script>
-	import { onMount } from 'svelte';
 	import Footer from '$components/Footer.svelte';
 	const Langs = [
 		{ name: '简体中文', url: 'https://zh.postal.wiki' },
 		{ name: 'English', url: 'https://en.postal.wiki' }
 	];
-
-	let stats = []; // 改为数组
-
-	onMount(async () => {
-		try {
-			const url =
-				'https://zh.postal.wiki/api.php?action=query&meta=siteinfo&siprop=statistics&format=json';
-			const response = await fetch(`${url}`); // 修复语法
-			const result = await response.json();
-			stats = [
-				{ name: 'Pages', value: result.query.statistics.pages },
-				{ name: 'Users', value: result.query.statistics.users },
-				{ name: 'Edits', value: result.query.statistics.edits },
-				{ name: 'Files', value: result.query.statistics.images }
-			];
-		} catch (error) {
-			console.error('获取统计数据失败:', error);
-		}
-	});
 </script>
 
 <!-- Main Container -->
-<div class="mx-auto max-w-6xl bg-white sm:pt-16 lg:pt-32">
+<div class="mx-auto max-w-6xl bg-white pt-16 lg:pt-32">
 	<h1 class="text-center text-4xl font-bold">Postal Wiki</h1>
 	<h2 class="pt-2 text-center text-2xl">A WIKI FOR POSTS</h2>
 	<!-- HyperLink to Different Language -->
@@ -39,7 +19,10 @@
 			>
 		{/each}
 	</div>
+
 	<!-- Statistic -->
+	<!-- [TODO] Statistics Fetch & Display -->
+	<!--
 	<h3 class="mt-12 text-center text-xl">Statistics</h3>
 	<dl
 		class="mt-8 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4"
@@ -53,6 +36,6 @@
 			</div>
 		{/each}
 	</dl>
-
+	-->
 	<Footer />
 </div>
