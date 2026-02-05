924c149bb2481a43f5eb83389321f416e1d7f1b4
jhl-hk <valor@jianyuelab.org>
2026-02-05 20:20:53 +0900

    feat: add analyse

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,15 @@
     <AppFooter />
   </UApp>
 </template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      defer: true,
+      src: "https://analyze.jianyuelab.co/script.js",
+      "data-website-id": "5f7bf075-4715-4bee-876d-687ca0297a84",
+    },
+  ],
+});
+</script>
